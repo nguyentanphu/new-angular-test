3c3205566a100f65cdfc4a47d4d01b9a6f585360
PhuNguyen <nguyentanphu@hotmail.com>
2020-02-29 10:41:22 +0700

    First message

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,121 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  Component,
+  Directive,
+  Renderer2,
+  HostListener,
+  HostBinding,
+  ElementRef,
+  NgModule,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+class Joke {
+  public setup: string;
+  public punchline: string;
+  public hide: boolean;
 
-if (environment.production) {
-  enableProdMode();
+  constructor(setup: string, punchline: string) {
+    this.setup = setup;
+    this.punchline = punchline;
+    this.hide = true;
+  }
+
+  toggle() {
+    this.hide = !this.hide;
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+@Directive({
+  selector: '[ccCardHover]'
+})
+class CardHoverDirective {
+  @Input('ccCardHover')
+  config = {
+    querySelector: '.card-text'
+  };
+
+  @HostBinding('attr.role') private ishovering: boolean;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
+  }
+
+  @HostListener('mouseover') onMouseOver() {
+    debugger;
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
+    this.renderer.setStyle(part, 'display', 'block');
+    this.ishovering = true;
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
+    this.renderer.setStyle(part, 'display', 'none');
+    this.ishovering = false;
+  }
+}
+
+@Component({
+  selector: 'joke',
+  template: `
+    <div class="card card-block" [ccCardHover]="{querySelector: '.card-text'}">
+      <h4 class="card-title">{{ data.setup }}</h4>
+      <p class="card-text" [style.display]="'none'">{{ data.punchline }}</p>
+    </div>
+  `
+})
+class JokeComponent {
+  @Input('joke') data: Joke;
+}
+
+@Component({
+  selector: 'joke-list',
+  template: `
+    <joke *ngFor="let j of jokes" [joke]="j"></joke>
+  `
+})
+class JokeListComponent {
+  jokes: Joke[];
+
+  constructor() {
+    this.jokes = [
+      new Joke(
+        'What did the cheese say when it looked in the mirror?',
+        'Hello-me (Halloumi)'
+      ),
+      new Joke(
+        'What kind of cheese do you use to disguise a small horse?',
+        'Mask-a-pony (Mascarpone)'
+      ),
+      new Joke(
+        'A kid threw a lump of cheddar at me',
+        'I thought ‘That’s not very mature’'
+      )
+    ];
+  }
+}
+
+@Component({
+  selector: 'app',
+  template: `
+    <joke-list></joke-list>
+  `
+})
+class AppComponent {}
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    JokeComponent,
+    JokeListComponent,
+    CardHoverDirective
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
